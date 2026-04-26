fafe85bf233fbc718dbf04949e18e502c435eabd
gmnayem631 <gmnayem631@gmail.com>
2026-04-26 11:12:27 +0600

    created a simple navigation bar

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-zinc-50 font-sans dark:bg-black text-4xl px-4 py-8">
+      Welcome to Dev Story
+    </div>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="border-b-2 border-zinc-500 px-4 py-3">
+      <h2 className="text-lg font-semibold">Dev Story</h2>
+    </header>
+  );
+};
+
+export default Header;
